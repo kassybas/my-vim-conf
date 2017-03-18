182d4598fb520157df28bb880f2e7fd6239e3973
kassybas <ubuntu@vag-vm>
2017-03-18 19:00:10 +0000

    added git annoyances

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 cp vimrc ~/.vimrc
 cp *.ttf /usr/share/fonts/
 cp %gconf.xml ~/.gconf/apps/gnome-terminal/profiles/
+git config --global core.editor "vim"
+git config --global alias.st status
+git config --global user.name "kassybas"
