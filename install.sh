1fd4652044b803ede967367683df5af1b0594931
Barna Kutassy <barnak@spotify.com>
2022-09-05 17:08:43 +0200

    Add vim-plug install

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 
+ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 brew uninstall ctags
 brew install universal-ctags sqlite fd ripgrep glow
 
